c1b233fc9dadcbb61d0404e8748dd937dfb62bcb
Aleksei <alexey.swn@gmail.com>
2026-07-21 09:37:49 +1000

    Update Next.js configuration: set output to 'export', disable React strict mode, and allow TypeScript to ignore build errors for improved build flexibility.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  reactStrictMode: false,
+  // experimental: {
+  //   scrollRestoration: false,
+  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
